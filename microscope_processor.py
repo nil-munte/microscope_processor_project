1fbada697763e90eee98c7fdf2e9a899c14441ad
Nil Munte <nilmunte@gmail.com>
2025-12-12 09:39:03 +0100

    Complete frame combination algorithm and fourier based demodulation

--- microscope_processor.py
+++ microscope_processor.py
@@ -1,11 +1,14 @@
 import tifffile
+import imageio.v2 as iio
 import numpy as np
+from scipy.signal import butter, filtfilt
 
 class MicroscopeProcessor:
 
-    def __init__(self, original_stack_img):
+    def __init__(self, original_stack_img, original_single_img):
         self.stack = original_stack_img
-        self.C, _, _ = self.stack.shape
+        self.img = original_single_img
+        self.C_stack, _, _ = self.stack.shape
 
     # Frame combination algorithm. Average projection: compute the mean across all C frames
     # Computing the sum across axis 0 (C dimension): I_result = Σ_{i=0}^{C-1} I_i(x, y)
@@ -21,14 +24,72 @@ class MicroscopeProcessor:
     # Frame combination algorithm. Weighted complex average: compute a weighted sum using complex exponential weights 𝑤𝑘 = exp 𝑖 𝑘 𝜋/𝐶 , and output the magnitude of the result. At the end, take the absolute value as result.
     def weighted_complex_average(self):
         # wk = exp(i · k · 2π / C)
-        # k is np.arange(self.c): array([0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
+        # k is np.arange(self.C_stack): array([0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
         # w_k has a final size of [C] Complex Values
-        w_k = np.exp(1j * np.arange(self.C) * 2 * np.pi / self.C)
+        w_k = np.exp(1j * np.arange(self.C_stack) * 2 * np.pi / self.C_stack)
         # w_k[:, None, None]: reshapes w_k from C to C, 1, 1 so we can multiply weights and image at each corresponding channel
         return np.abs(np.sum(self.stack * w_k[:, None, None], axis = 0))
+    
+    # Fourier-based demodulation method
+    # Butterworth filter (2D), indicating type ('high', 'low' for this project) (private method)
+    # The order of a Butterworth filter controls how steeply the filter transitions from passband to stopband
+    def _butter_filter(self, cuttoff_frequency, order, type):
+        # The cutoff frequencies in this method are normalized by the Nyquist frequency
+        Wn = cuttoff_frequency/0.5
+        b, a = butter(order, Wn, btype=type, analog=False)
+        return b, a
+    
+    # Fourier-based demodulation method
+    # Butterworth filter application to the image (private method)
+    def _apply_filter(self, img, b, a):
+        # Apply the 2-D Vutterworth filter along both axes using filtfilt for zero-phase filtering
+        # axis = 0 -> H
+        # axis = 1 -> W
+        img_filt_x = filtfilt(b, a, img, axis=0)
+        img_filt_xy = filtfilt(b, a, img_filt_x, axis=1)
+        
+        return img_filt_xy
+    
+    # Fourier-based demodulation method
+    def fourier_based_demodulation(self, T, order):
+        
+        cut_off_frequency = 1 / T
+        
+        # 1) High-pass filtering
+        bh, ah = self._butter_filter(cut_off_frequency, order, 'highpass')
+        high_pass_filtered_img = self._apply_filter(self.img, bh, ah)
+        
+        # 2) Frequency downshift via multiplication by sine/cosine references
+        # Modulation along width
+        x = np.arange(self.img.shape[0])[:, None]
+        # Cosine modulation of period T
+        cos_mod = np.cos(2*np.pi*x / T)
+        # Sine modulation of period T
+        sin_mod = np.sin(2*np.pi*x / T)
+        
+        A_mix_img = high_pass_filtered_img * cos_mod
+        B_mix_img = high_pass_filtered_img * sin_mod
+        
+        # 3) Low-pass filtering of the A and B signals
+        # Retains only the frequency content of interest while discarding high-frequency artifacts
+        bl, al = self._butter_filter(cut_off_frequency, order, 'lowpass')
+        A_low_pass_img = self._apply_filter(A_mix_img, bl, al)
+        B_low_pass_img = self._apply_filter(B_mix_img, bl, al)
+        
+        # 4) Magnitude reconstruction
+        # Combine the filtered A and B components:
+        img_result = np.sqrt(A_low_pass_img**2 + B_low_pass_img**2)
+
+        return high_pass_filtered_img, A_mix_img, B_mix_img, A_low_pass_img, B_low_pass_img, img_result
     
     # Method to load a TIFF into a numpy array
     # Input shape is (C, H, W), with C = 10
     @staticmethod
     def load_tif(tiff_file_path):
         return tifffile.imread(tiff_file_path)
+    
+    # Method to load a PNG image into a numpy array
+    # Input shape is (H, W)
+    @staticmethod
+    def load_png(png_file_path):
+        return iio.imread(png_file_path)
--- run_processing.py
+++ run_processing.py
@@ -2,8 +2,9 @@ from microscope_processor import MicroscopeProcessor
 from matplotlib import pyplot as plt
 import os
 
-# Input image
+# Input images
 input_tif_image = "input_images/background_removal_raw.tif"
+input_png_image = "input_images/a.png"
 
 # Output image folder
 output_folder = "output_images/"
@@ -11,10 +12,12 @@ os.makedirs(output_folder, exist_ok=True)
 
 # Importing the original TIF image
 original_stack_img = MicroscopeProcessor.load_tif(input_tif_image)
-# Creating an instance of the MicroscopeProcessor class
-processor = MicroscopeProcessor(original_stack_img)
+original_single_img = MicroscopeProcessor.load_png(input_png_image)
 
-# Frame combination algorithm modes
+# Creating an instance of the MicroscopeProcessor class
+processor = MicroscopeProcessor(original_stack_img, original_single_img)
+
+# ------------ Frame combination algorithm 
 
 # Average projection
 avg_projection_img = processor.average_projection()
@@ -25,9 +28,20 @@ min_max_projection_img = processor.max_min_projection()
 # Weighted complex average
 weighted_complex_avg_img = processor.weighted_complex_average()
 
-# Plot processed images
-plt.figure(figsize=(12,4))
+# ------------ Fourier-based demodulation
 
+# Period (in pixels)
+T = 16
+# Order: between 3-4 to obtain a good balance between removing background and preserving image details
+order = 3
+
+high_pass_filtered_img, A_mix_img, B_mix_img, A_low_pass_img, B_low_pass_img, fourier_based_img = processor.fourier_based_demodulation(T, order)
+
+# ------------ Plot processed images
+
+plt.figure(1, figsize=(12,4))
+
+# Frame combination algorithm
 plt.subplot(1,3,1)
 plt.imshow(avg_projection_img, cmap='gray')
 plt.title("Average Projection")
@@ -40,9 +54,43 @@ plt.subplot(1,3,3)
 plt.imshow(weighted_complex_avg_img, cmap='gray')
 plt.title("Weighted Complex Average")
 
+# Fourier-based demodulation
+
+plt.figure(2, figsize=(12,4))
+
+plt.subplot(2,3,1)
+plt.imshow(high_pass_filtered_img, cmap='gray')
+plt.title("High-Pass Filtered Image")
+
+plt.subplot(2,3,2)
+plt.imshow(A_mix_img, cmap='gray')
+plt.title("A_mix Image")
+
+plt.subplot(2,3,3)
+plt.imshow(B_mix_img, cmap='gray')
+plt.title("B_mix Image")
+
+plt.subplot(2,3,4)
+plt.imshow(A_low_pass_img, cmap='gray')
+plt.title("Low-Pass Filtered A_mix Image")
+
+plt.subplot(2,3,5)
+plt.imshow(B_low_pass_img, cmap='gray')
+plt.title("Low-Pass Filtered B_mix Image")
+
+plt.subplot(2,3,6)
+plt.imshow(fourier_based_img, cmap='gray')
+plt.title("Fourier-Based Demodulation Image")
+
 plt.show()
 
-# Save processed images
+# ------------ Save processed images
 plt.imsave(output_folder + "avg_projection_img.png", avg_projection_img, cmap='gray')
 plt.imsave(output_folder+ "min_max_projection_img.png", min_max_projection_img, cmap='gray')
 plt.imsave(output_folder + "weighted_complex_avg_img.png", weighted_complex_avg_img, cmap='gray')
+plt.imsave(output_folder + "high_pass_filtered_img.png", high_pass_filtered_img, cmap='gray')
+plt.imsave(output_folder+ "A_mix_img.png", A_mix_img, cmap='gray')
+plt.imsave(output_folder + "B_mix_img.png", B_mix_img, cmap='gray')
+plt.imsave(output_folder + "A_low_pass_img.png", A_low_pass_img, cmap='gray')
+plt.imsave(output_folder+ "B_low_pass_img.png", B_low_pass_img, cmap='gray')
+plt.imsave(output_folder + "fourier_based_img.png", fourier_based_img, cmap='gray')
